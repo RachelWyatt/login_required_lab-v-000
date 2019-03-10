782ce0802f3f309af0e0d7592db7da840f7eb2e3
Rachel Wyatt <rw126@evansville.edu>
2019-03-10 14:32:42 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user 
-    if session[:user_id]
-      User.find_by(id: session[:user_id])
-    end
+    User.find_by(id: session[:user_id])
   end
 end
